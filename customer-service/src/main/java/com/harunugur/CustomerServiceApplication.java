a878c7262e4eeb206f528acbfd8f625da84bc07a
Harun <ugur.harun@yandex.com>
2020-12-06 22:40:21 +0300

    fix dependencies

--- customer-service/src/main/java/com/harunugur/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/harunugur/CustomerServiceApplication.java
@@ -1,15 +1,15 @@
 package com.harunugur;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableAdminServer
-@Configuration
+@EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerServiceApplication.class);

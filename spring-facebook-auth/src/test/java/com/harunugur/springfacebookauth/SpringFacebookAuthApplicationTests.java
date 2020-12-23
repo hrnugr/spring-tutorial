6c3341765d100b39b91123b4df386cd74b758d93
Harun <ugur.harun@yandex.com>
2020-12-24 00:24:06 +0300

    fb auth2

---
+++ spring-facebook-auth/src/test/java/com/harunugur/springfacebookauth/SpringFacebookAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harunugur.springfacebookauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFacebookAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
